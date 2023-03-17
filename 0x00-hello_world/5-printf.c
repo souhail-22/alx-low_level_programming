c0cf3019cb9afdb26b18c6e5a1ee25a4d22d6a1f
SOUHAIL HAMIDI <93973523+souhail-22@users.noreply.github.com>
2023-03-17 20:14:03 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-
-/**
-*main - Entry point 
-*Return: Always 0 (Success)
-*/
-
 int main (void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
-	return(0);
+printf("\"Programming is like building a multilingual puzzle");
+return(0);
 
 }
